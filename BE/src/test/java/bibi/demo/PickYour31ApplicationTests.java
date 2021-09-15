736229a02c2b8de0163c930923c6043850569b91
bibi <bibi6666667@kakao.com>
2021-09-15 22:48:12 +0900

    initial commit

---
+++ BE/src/test/java/bibi/demo/PickYour31ApplicationTests.java
@@ -0,0 +1,13 @@
+package bibi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickYour31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
