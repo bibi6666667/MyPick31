85eafd7d7381f44dc4be6ec77356de953fe67376
bibi <bibi6666667@kakao.com>
2021-09-16 14:56:42 +0900

    프로젝트명 변경

---
+++ BE/src/main/java/bibi/demo/MyPick31.java
@@ -0,0 +1,13 @@
+package bibi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickYour31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickYour31Application.class, args);
+	}
+
+}
