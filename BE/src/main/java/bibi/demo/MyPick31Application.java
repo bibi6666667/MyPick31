174bbc11a7a147fdc2db1affe9eb16175c4aefdb
bibi <bibi6666667@kakao.com>
2021-09-24 18:04:31 +0900

    initial commit

--- BE/src/main/java/bibi/demo/MyPick31Application.java
+++ BE/src/main/java/bibi/demo/MyPick31Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PickYour31Application {
+public class MyPick31Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PickYour31Application.class, args);
+		SpringApplication.run(MyPick31Application.class, args);
 	}
 
 }
