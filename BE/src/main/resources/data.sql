60d583fe71ca0cf120785ae609ae1c5a6dfc4a67
bibi <bibi6666667@kakao.com>
2021-09-29 21:49:04 +0900

    feat : data.sql - base_type, topping_type, syrup_type, allergen 데이터 삽입 구문 작성

---
+++ BE/src/main/resources/data.sql
@@ -0,0 +1,62 @@
+insert into `base_type` (name_kr, name_en)
+values
+("바닐라", "Vanilla"),
+("초콜릿", "Chocolate"),
+("치즈", "Cheese"),
+("민트", "Mint"),
+("녹차", "Green tea"),
+("과일", "Fruit"),
+("요거트", "Yogurt"),
+("견과류", "Nut"),
+("커피", "Coffee"),
+("카라멜", "Caramel"),
+("솜사탕", "Cotton candy"),
+("우유", "Milk"),
+("크림", "Cream"),
+("과자", "Snack foods"),
+("곡류/서류", "Grain/Tuber"),
+("빵/떡", "Bread/Rice cake"),
+("음료", "Beverage"),
+("사탕", "Candy");
+
+insert into `topping_type` (name_kr, name_en)
+values
+("과일", "Fruit"),
+("초콜릿", "Chocolate"),
+("초콜릿 칩", "Chocolate chip"),
+("초코볼", "Chocolate ball"),
+("견과류", "Nut"),
+("크런치", "Crunchy"),
+("크런치 초콜릿", "Crunchy chocolate"),
+("쿠키", "Cookie"),
+("케이크", "Cake"),
+("과자", "Snack foods"),
+("곡류/서류", "Grain/Tuber"),
+("빵/떡", "Bread/Rice cake"),
+("사탕", "Candy"),
+("젤리", "Jelly");
+
+insert into `syrup_type` (name_kr, name_en)
+values
+("초콜릿", "Chocolate"),
+("카라멜", "Caramel"),
+("과일", "Fruit"),
+("꿀", "Honey"),
+("연유/크림", "Condensed milk/Cream"),
+("과자", "Snack foods"),
+("곡류/서류", "Grain/Tuber"),
+("빵/떡", "Bread/Rice cake");
+
+insert into `allergen` (name_kr, name_en)
+values
+("우유", "Milk"),
+("대두", "Soybeans"),
+("밀", "Wheat"),
+("계란", "Eggs"),
+("땅콩", "Peanuts"),
+("호두", "Walnuts"),
+("복숭아", "Peaches"),
+("오징어", "Squid"),
+("쇠고기", "Beef"),
+("조개류", "Shellfish");
+
