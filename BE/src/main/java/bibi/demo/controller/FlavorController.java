7390473d9488d1f97b708b5c50ae801bdf3d29d7
bibi <bibi6666667@kakao.com>
2021-10-13 22:27:11 +0900

    chore : import 통합 및 삭제 #9

--- BE/src/main/java/bibi/demo/controller/FlavorController.java
+++ BE/src/main/java/bibi/demo/controller/FlavorController.java
@@ -2,10 +2,7 @@ package bibi.demo.controller;
 
 import bibi.demo.response.FlavorResponse;
 import bibi.demo.service.FlavorService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
