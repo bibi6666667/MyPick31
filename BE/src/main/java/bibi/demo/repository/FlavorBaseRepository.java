cbfd9c4e4970c601c6ef01c59bb586a8b44d6789
bibi <bibi6666667@kakao.com>
2021-10-18 22:27:25 +0900

    feat : 기존 메서드를 같은 기능의 쿼리메소드로 대체 #12

--- BE/src/main/java/bibi/demo/repository/FlavorBaseRepository.java
+++ BE/src/main/java/bibi/demo/repository/FlavorBaseRepository.java
@@ -1,9 +1,7 @@
 package bibi.demo.repository;
 
 import bibi.demo.domain.flavor.FlavorBase;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +9,5 @@ import java.util.List;
 @Repository
 public interface FlavorBaseRepository extends CrudRepository<FlavorBase, Long> {
 
-    @Query(value = "SELECT fb.id, fb.flavor_id, fb.base_id FROM flavor_base fb WHERE fb.base_id = :baseId", nativeQuery = true)
-    List<FlavorBase> findFlavorBasesByBaseId(@Param("baseId") Long baseId);
+    List<FlavorBase> findByBaseId(Long baseId);
 }
