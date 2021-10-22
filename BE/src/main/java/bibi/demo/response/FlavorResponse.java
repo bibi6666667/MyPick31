adb76e146d454ecf8e05a44484ccc9c1cd1bdaa8
bibi <bibi6666667@kakao.com>
2021-10-23 00:07:27 +0900

    feat : FlavorResponse에 pickNum 추가 #17
    flavorResponse에 플레이버가 받은 픽 수를 뜻하는 pickNum을 추가

--- BE/src/main/java/bibi/demo/response/FlavorResponse.java
+++ BE/src/main/java/bibi/demo/response/FlavorResponse.java
@@ -19,10 +19,12 @@ public class FlavorResponse {
     private List<ToppingResponse> toppingList;
     private List<SyrupResponse> syrupList;
     private List<AllergenResponse> allergenList;
+    private Long pickNum;
 
     public FlavorResponse(Long id, String nameKR, String nameEN, int kcal, boolean isSignature,
                           InfoResponse info, ImageResponse image, OnSaleResponse onSale,
-                          List<BaseResponse> baseList, List<ToppingResponse> toppingList, List<SyrupResponse> syrupList, List<AllergenResponse> allergenList) {
+                          List<BaseResponse> baseList, List<ToppingResponse> toppingList,
+                          List<SyrupResponse> syrupList, List<AllergenResponse> allergenList, Long pickNum) {
         this.id = id;
         this.nameKR = nameKR;
         this.nameEN = nameEN;
@@ -35,5 +37,6 @@ public class FlavorResponse {
         this.toppingList = toppingList;
         this.syrupList = syrupList;
         this.allergenList = allergenList;
+        this.pickNum = pickNum;
     }
 }
