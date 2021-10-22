0fb448d2c6c7627e54555f00e7cf41626b3d020c
bibi <bibi6666667@kakao.com>
2021-10-22 23:24:37 +0900

    feat : PickRepository 및 CRUD메소드 구현 #17
    오버라이딩 및 쿼리메소드를 활용해 Pick CRUD에 필요한 메서드 구현

---
+++ BE/src/main/java/bibi/demo/repository/PickRepository.java
@@ -0,0 +1,31 @@
+package bibi.demo.repository;
+
+import bibi.demo.domain.Pick;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PickRepository extends CrudRepository<Pick, Long> {
+    @Override
+    <S extends Pick> S save(S entity);
+
+    @Override
+    void delete(Pick entity);
+
+    @Override
+    void deleteById(Long pickId);
+
+    Long countByFlavorId(Long flavorId);
+
+
+    List<Pick> findByUserId(Long userId);
+
+    Optional<Pick> findByFlavorIdAndUserId(Long flavorId, Long userId);
+
+    boolean existsByFlavorIdAndUserId(Long flavorId, Long userId);
+
+    boolean existsById(Long pickId);
+}
